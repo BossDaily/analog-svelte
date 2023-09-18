848907fe8287ab82747a603e30c735bba7bb0c75
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-09-18 17:25:13 +0000

    Initial commit

---
+++ apps/dashboard/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { signIn, signOut } from "@auth/sveltekit/client"
+  import { page } from "$app/stores"
+</script>
+
+<h1 >Welcome to Analog-Svelte</h1>
+<p>
+  {#if $page.data.session}
+    <p>You're signed in</p>
+    <button on:click={signOut}>Sign out</button>
+  {:else}
+    <p>You need to sign in</p>
+    <button on:click={signIn}>Sign in</button>
+  {/if}
+</p>
